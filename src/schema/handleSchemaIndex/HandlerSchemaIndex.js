6953299fb0731b8bcae889445731b2d4b206ec0c
Mohammad.Khalid95 <muhammad.khalid@bykea.com>
2019-09-25 11:49:05 +0500

    added comments in handlerSchemaIndex function

--- src/schema/handleSchemaIndex/HandlerSchemaIndex.js
+++ src/schema/handleSchemaIndex/HandlerSchemaIndex.js
@@ -6,15 +6,19 @@ module.exports = class HandlerSchemaIndex {
         this.indexPath = 'body';
     }
 
+/*
+  * This method is use to get and validate schema of body and header from their respective folder and file.
+*/
+
     getSchema(_class, body){
-        const [name, schema] = _class.constructor.name.split('Handler');
+        const [name, schema] = _class.constructor.name.split('Handler'); //Getting name and schema from class from which this function called e.g User and Schema
         
-        const className = `${name}Handler${schema}Index`;
+        const className = `${name}Handler${schema}Index`; // Creating a className by joining name and schema e.g UserHandlerSchemaIndex 
         
-        const requiredClass = require(path.join(__dirname,this.indexPath,className));
+        const requiredClass = require(path.join(__dirname,this.indexPath,className)); // Creating a required class to call a specific validation function.
 
-        const bodyIndex = new requiredClass(this.Joi);
+        const bodyIndex = new requiredClass(this.Joi); // Passing Joi Validator to the Class.
 
-        return bodyIndex[body]();
+        return bodyIndex[body](); // Calling Validation function to validate the body e.g create()
     };
 };
